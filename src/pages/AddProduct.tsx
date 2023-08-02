f594f6a54098e527eb5b595d72a1762262d79a67
Michał Jasiński <michaqu11@wp.pl>
2023-08-02 02:03:49 +0200

    added functionality to changing mealName after click in menu

--- src/pages/AddProduct.tsx
+++ src/pages/AddProduct.tsx
@@ -1,6 +1,6 @@
 import Paper from "@mui/material/Paper";
 import useMediaQuery from "@mui/material/useMediaQuery";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import "./Page.scss";
 import SearchInput from "../components/elements/products/search/SearchInput";
@@ -11,18 +11,24 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 
 const AddProduct: React.FC = () => {
-  let { id } = useParams();
+  // let { id } = useParams();
   const location = useLocation();
   const { meal } = location.state;
   const { days } = location.state;
   const Mobile = useMediaQuery("(min-width:700px)");
 
+  const [mealName, setMealName] = useState<String>(meal);
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
   const open = Boolean(anchorEl);
 
   const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     setAnchorEl(event.currentTarget);
+  };
+
+  const close = (e: String) => {
+    setMealName(e);
+    setAnchorEl(null);
   };
 
   const handleClose = () => {
@@ -32,26 +38,30 @@ const AddProduct: React.FC = () => {
   return (
     <>
       <Paper elevation={0} sx={{ margin: "5px" }}>
-      <Grid container justifyContent="flex-start">
-            <Grid>
-              <IconButton color="primary" component={Link} to={`/`} aria-label="back">
-                <ArrowBackIosNewIcon />
-              </IconButton>
-            </Grid>
-            <Grid sx={{marginTop: "2px"}}>
-              <Button
-                id="basic-button"
-                aria-controls={open ? "basic-menu" : undefined}
-                aria-haspopup="true"
-                aria-expanded={open ? "true" : undefined}
-                onClick={handleClick}
-              >
-                {meal}
-              </Button>
-            </Grid>
+        <Grid container justifyContent="flex-start">
+          <Grid>
+            <IconButton
+              color="primary"
+              component={Link}
+              to={`/`}
+              aria-label="back"
+            >
+              <ArrowBackIosNewIcon />
+            </IconButton>
           </Grid>
+          <Grid sx={{ marginTop: "2px" }}>
+            <Button
+              id="basic-button"
+              aria-controls={open ? "basic-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              {mealName}
+            </Button>
+          </Grid>
+        </Grid>
         <div className={!Mobile ? "container-mobile" : "container-desktop"}>
-
           <SearchInput />
           <Divider sx={{ marginTop: "10px" }} />
         </div>
@@ -66,9 +76,10 @@ const AddProduct: React.FC = () => {
         }}
       >
         {days
-          .filter((e: String) => e !== meal)
-          .map((e: String, index: number) => (<div key={index}>
-            <MenuItem onClick={handleClose}>{e}</MenuItem>
+          .filter((e: String) => e !== mealName)
+          .map((e: String, index: number) => (
+            <div key={index}>
+              <MenuItem onClick={() => close(e)}>{e}</MenuItem>
             </div>
           ))}
       </Menu>
