1b32ad53b1cdfe6ad497ae10f281ef0827f361a9
Michał Jasiński <michaqu11@wp.pl>
2023-09-06 23:25:22 +0200

    added service in java spring boot

---
+++ diabefit-service/src/main/java/com/diafite/DiabefitServiceApplication.java
@@ -0,0 +1,13 @@
+package com.diafite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabefitServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiabefitServiceApplication.class, args);
+    }
+
+}
---
+++ diabefit-service/src/test/java/com/diafite/DiabefitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diafite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabefitServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
