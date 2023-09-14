15709589b032d7d53eaa8bf8e64b0e8d9c887326
Michał Jasiński <michaqu11@wp.pl>
2023-09-14 22:29:57 +0200

    add searching products mechanism from fatsecret api

--- diabefit-ui/src/api/fatsecret-api.ts
+++ diabefit-ui/src/api/fatsecret-api.ts
@@ -1,13 +1,13 @@
-import axios from 'axios'
-import OAuth from 'oauth-1.0a';
-import * as CryptoJS from 'crypto-js';
+import axios from "axios";
+import OAuth from "oauth-1.0a";
+import * as CryptoJS from "crypto-js";
 
 const oauth = new OAuth({
   consumer: {
-      key: 'a10b021da4fa4e4abaf91c1f047ea9c6',
-      secret: '712c2646d33a48caa69ddb4a5574febf',
+    key: "a10b021da4fa4e4abaf91c1f047ea9c6",
+    secret: "712c2646d33a48caa69ddb4a5574febf",
   },
-  signature_method: 'HMAC-SHA1',
+  signature_method: "HMAC-SHA1",
   hash_function(baseString, key) {
     const hmac = CryptoJS.HmacSHA1(baseString, key);
     return CryptoJS.enc.Base64.stringify(hmac);
@@ -16,53 +16,55 @@ const oauth = new OAuth({
 
 const requestData = (method: string, params: any) => {
   return {
-  url: 'https://platform.fatsecret.com/rest/server.api',
-  method: 'GET',
-  data: {
-    method: method, 
-    format: 'json',
-    oauth_signature_method: 'HMAC-SHA1',
-    ...params
-  },
-}};
+    url: "https://platform.fatsecret.com/rest/server.api",
+    method: "GET",
+    data: {
+      method: method,
+      format: "json",
+      oauth_signature_method: "HMAC-SHA1",
+      max_results: '50',
+      ...params,
+    },
+  };
+};
 
-export const searchFood = async(foodName: string) => {
-
-      const data = requestData('foods.search.v2', { search_expression: foodName})
-      const params = oauth.authorize(data)
-
-      return axios({
-        method: data.method,
-        url: data.url,
-        params:params 
-        
-      })
-        .then((response) => {
-          return response.data; 
-        })
-        .catch((error) => {
-          console.error(error); 
-          return undefined;
-        });
-
-}
-
-export const autocomplete = async(searchExpresion: string) => {
-
-  const data = requestData('foods.autocomplete.v2', { expression: searchExpresion})
-  const params = oauth.authorize(data)
+export const searchFood = async (foodName: string, pageNumber: number) => {
+  const data = requestData("foods.search.v2", {
+    search_expression: foodName,
+    page_number: pageNumber,
+  });
+  const params = oauth.authorize(data);
 
   return axios({
     method: data.method,
     url: data.url,
-    params:params 
-    
+    params: params,
   })
     .then((response) => {
-      return response.data; 
+      return response.data;
     })
     .catch((error) => {
-      console.error(error); 
+      console.error(error);
       return undefined;
     });
-}
+};
+
+export const autocomplete = async (searchExpresion: string) => {
+  const data = requestData("foods.autocomplete.v2", {
+    expression: searchExpresion,
+  });
+  const params = oauth.authorize(data);
+
+  return axios({
+    method: data.method,
+    url: data.url,
+    params: params,
+  })
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      console.error(error);
+      return undefined;
+    });
+};
