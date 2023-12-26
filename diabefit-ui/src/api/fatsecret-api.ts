8aeff5842b3d03cac6c114204c325005dd03255f
Michał Jasiński <michaqu11@wp.pl>
2023-12-26 12:34:55 +0100

    fix url typo

--- diabefit-ui/src/api/fatsecret-api.ts
+++ diabefit-ui/src/api/fatsecret-api.ts
@@ -4,7 +4,7 @@ import { SERVICE_URL } from "../config/data";
 
 export const searchFood = async (foodName: string, pageNumber: number) => {
   const { data } = await axios.post(
-    SERVICE_URL + `search?name=${foodName}&page=${pageNumber}`,
+    SERVICE_URL + `/search?name=${foodName}&page=${pageNumber}`,
     {
       headers: {
         "Access-Control-Allow-Origin": "*",
--- diabefit-ui/src/config/data.ts
+++ diabefit-ui/src/config/data.ts
@@ -1,2 +1,2 @@
-export const SERVICE_URL = "https://diabefit.pythonanywhere.com/";
+export const SERVICE_URL = "https://diabefit.pythonanywhere.com";
 export const LIBRE_API_URL = "https://api-eu.libreview.io/llu/connections";
